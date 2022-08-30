b992be73b73485e123269bca110f0f070aa2cf9f
Ethan Pierce <ethanpiercepresents@gmail.com>
2022-08-29 23:50:21 -0700

    updating header margin

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
+import React from "react";
 import {
-  Box,
-  UnorderedList,
-  ListItem,
-  Image,
-  Heading,
-  Button,
-} from '@chakra-ui/react';
-import NavLink from '../styles/custom/NavLink';
-import Link from 'next/link';
-import { useSession, signIn, signOut } from 'next-auth/react';
+	Box,
+	UnorderedList,
+	ListItem,
+	Image,
+	Heading,
+	Button,
+} from "@chakra-ui/react";
+import NavLink from "../styles/custom/NavLink";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Header = () => (
-  <Box
-    display='flex'
-    justifyContent='space-between'
-    alignItems='center'
-    h='80px'
-    bgColor='whiteAlpha.700'
-    bgGradient='linear(gray.900 0%, blue.900 20%)'
-    px={6}
-  >
-    <Link href='/' passHref>
-      <Heading mx={4} color='whiteAlpha.900' _hover={{ cursor: 'pointer' }}>
-        PC
-      </Heading>
-    </Link>
+	<Box
+		display="flex"
+		justifyContent="space-between"
+		alignItems="center"
+		h="80px"
+		bgColor="whiteAlpha.700"
+		bgGradient="linear(gray.900 0%, blue.900 20%)"
+		px={[0, 80, 100, 150]}
+	>
+		<Link href="/" passHref>
+			<Heading mx={4} color="whiteAlpha.900" _hover={{ cursor: "pointer" }}>
+				PC
+			</Heading>
+		</Link>
 
-    <UnorderedList
-      listStyleType='none'
-      display='flex'
-      justifyContent='space-evenly'
-      color='whiteAlpha.900'
-    >
-      <ListItem>
-        <NavLink to='/main'>Home</NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to='/about'>About</NavLink>
-      </ListItem>
-      <ListItem>
-        <NavLink to='/cancel'>Cancel</NavLink>
-      </ListItem>
-    </UnorderedList>
-  </Box>
+		<UnorderedList
+			listStyleType="none"
+			display="flex"
+			justifyContent="space-evenly"
+			color="whiteAlpha.900"
+		>
+			<ListItem>
+				<NavLink to="/main">Home</NavLink>
+			</ListItem>
+			<ListItem>
+				<NavLink to="/about">About</NavLink>
+			</ListItem>
+			<ListItem>
+				<NavLink to="/cancel">Cancel</NavLink>
+			</ListItem>
+		</UnorderedList>
+	</Box>
 );
 
 export default Header;
